090f950625d2d99ef53f41b999721ee18da27423
Ian O'Hara <ian.ohara@gmail.com>
2012-01-29 13:29:12 -0800

    sim_test is now standalone, and added run_sim to take care of stepping a simulator structure forward a certain number of steps.

---
+++ sim/run_sim.m
@@ -0,0 +1,19 @@
+function sim = run_sim(sim, steps)
+% Step a simulation through a certain number of steps.
+% 
+% ARGUMENTS
+%   sim - Simulator structure to run
+%   steps - Number of steps to run the simulator for
+% RETURNS
+%   sim - Simulator structure with sim.q and sim.qd
+%         filled in.
+%
+
+for i = 1:steps-1
+    [q_n,qd_n] = step_sim(sim.chain, sim.q(:,i), sim.qd(:,i), sim.T(:,i), sim.dt);
+    sim.q(:,i+1) = q_n;
+    sim.qd(:,i+1) = qd_n;
+    sim.s = sim.s+1;
+end
+
+end
--- tests/sim_test.m
+++ tests/sim_test.m
@@ -6,21 +6,25 @@
 %    -> Equiv to a pendulum
 clear all
 close all
+
 chain_single = [
     new_link('HT1','rotate', rotY(pi/2));
-    new_link('HT2');
     new_link('HT1');
-    new_link('HT_head_cap');
+    new_link('HT1');
+    new_link('HT2');
+    %new_link('HT_head_cap');
     ];
 
 N = size(chain_single, 1);
 
-t_sim = 30;    % Simulate for t_sim [s]
-num_s = 3000;  % Use this many timesteps
+t_sim = 10;    % Simulate for t_sim [s]
+num_s = 1000;  % Use this many timesteps
 
 torque_history = zeros(N,num_s);  % At each timestep, the torque of each motor needs to be specified
 q0 = zeros(N,1);
-q0(2,1) = pi/6;
+q0(1,1) = 0;
+q0(3,1) = pi/6;
+q0(4,1) = -pi/6;
 
 qd0 = zeros(N,1);
 
@@ -28,12 +32,6 @@ sim = new_sim('steps', num_s, 'sim_time', t_sim, 'chain', chain_single, ...
     'torques', torque_history, 'q0', q0, 'qd0', qd0);
 
 tic;
-% Calculate the rate of change of state at these initial conditions
-for i=1:num_s-1
-    [q_n,qd_n] = step_sim(sim.chain, sim.q(:,i), sim.qd(:,i), sim.T(:,i), sim.dt);
-    sim.q(:,i+1) = q_n;
-    sim.qd(:,i+1) = qd_n;
-    sim.s = sim.s+1;
-end
+sim = run_sim(sim, num_s);
 toc
-%% 
+draw_sim(sim);
