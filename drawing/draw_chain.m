ed4f194b1820451804d6fdd9817ccc707d1b3778
Ian O'Hara <ian.ohara@gmail.com>
2012-01-29 12:39:38 -0800

    Documentation

--- drawing/draw_chain.m
+++ drawing/draw_chain.m
@@ -2,16 +2,35 @@ function success = draw_chain(chain, varargin)
 %  Draws a chain of connected CKBot links in the current figure
 %  
 %  ARGUMENTS:
-%    chain - vector of link structures in the order of their linking
+%    chain - vector of link structures in the order of their linking\
+%    varargin - See below
 %
 %  RETURNS:
 %    success - 1 for success, 0 for failure
 success = 0;
 
+% Hackery for checking varargins.  This works pretty well
+% for flag style options though.
+
+% Don't plot the patch derived CKBots (They're slow as hell, so this is
+% nice for "real time" plotting)
 if (sum(ismember(varargin, 'just_vectors')) > 0)
     just_vectors = 1;
 else
     just_vectors = 0;
+end
+
+% For using draw_chain as a standalone function.
+if (sum(ismember(varargin, 'need_figure')) > 0)
+    fprintf('Initializing a figure for you...\n');
+    figure;
+    hold on;
+    grid on;
+    axis equal;
+    xlabel('X [m]','FontSize',14);
+    ylabel('Y [m]','FontSize',14);
+    zlabel('Z [m]','FontSize',14);
+    view(45,45);
 end
 
 r_base = [0; 0; 0];  % First chain link starts at the origin
