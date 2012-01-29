1f6e7cfd2917a9dbefc7e88a8c045162777be61b
Ian O'Hara <ian.ohara@gmail.com>
2012-01-29 12:22:06 -0800

    Comments explaining what each calculation is in the tip to base recursion.

--- sim/tip_base_step.m
+++ sim/tip_base_step.m
@@ -19,6 +19,10 @@ function [G_all, mu_all, a_all] = tip_base_step(chain, s, T)
 %      and the 3 linear components are second.
 %
 % TODO:
+%   get_rotation... and get_omega... are eating up ~25% of computation time
+%   in this function.  They should be moved outside of the loop and changed
+%   so that they calculate the values for the entire arm at once, then
+%   index to the correct portions within the loop.
 % 
 
 grav = [0;0;0;0;0;9.81];
@@ -48,35 +52,50 @@ for i = N:-1:1
     r_i_ip = R_cur*(cur.r_ip1 - cur.r_im1); % Vector from current joint to outbound joint
     phi = get_bod_trans(r_i_ip);  % Spatial transform operator from this joint to outbound joint
     
+    % Vector from inbound joint to CM of this link (in inertial coords)
     r_i_cm = R_cur*(cur.r_im1);
+    % Spatial transformation from inbound joint to CM
     phi_cm = get_bod_trans(r_i_cm);
         
     M = get_spatial_inertia_mat(cur);  % 6x6 inertia matrix about inbound joint
     
     p_ind = get_block_indicies(i);
     
+    % Articulated body spatial inertia of this link
     p_cur = phi*pp*phi' + M;
     
+    % Joint matrix.  H is in the inertial coordinate sys (like everything
+    % used in this algorithm should be)
     H_b_frame_star = get_joint_mat(cur)';
     H_w_frame_star = [R_cur, zeros(3);zeros(3),eye(3)]*H_b_frame_star;
     H = H_w_frame_star';
     
+    % Joint "Inertia"
     D = H*p_cur*H';
+    
     G = p_cur*H'*(1/D);
     
     tau_tilde = eye(6) - G*H;
-    pp = tau_tilde*p_cur; % Set p+ for next time around loop
+    
+    % Set p+ for next time around loop
+    pp = tau_tilde*p_cur; 
     
     omega = get_angular_vel(chain, i, qd(i));
     omega_cross = get_cross_mat(omega);
     
+    % The Coriolis and Gyroscopic terms
     b = [omega_cross*chain(i).I_cm*omega;...
         chain(i).m*omega_cross*omega_cross*(-cur.r_im1)];
     a = [0; 0; 0; omega_cross*omega_cross*(-cur.r_im1)];
     
+    % Spatial compensation force
     z = phi*zp + p_cur*a+b + phi_cm*chain(i).m*grav;
+    
+    % Correction force through the inbound joint (including the torque
+    % added due to motor torque)
     epsilon = T(i) - H*z;
     
+    % Relative joint acceleration
     mu = (1/D)*epsilon;
     zp = z + G*epsilon; % Setting z+ for the next time around loop
 
@@ -85,22 +104,6 @@ for i = N:-1:1
     mu_all(i) = mu;
     a_all(p_ind) = a;
     
-%     if (exist('DEBUG_MSG') || exist('DEBUG_TIP_BASE'))
-%         fprintf('--------- Link %d -----------\n', i)
-%         fprintf('Chain Description:\n  (%d=tip, 1=base)\n', N);
-%         fprintf('The correction force at this link''s joint is:\n');
-%         print_vec(z);
-%         fprintf('The correction force through to the next link is:\n');
-%         print_vec(zp);
-%         fprintf('The Joint matrix is:\n');
-%         print_vec(H')
-%         fprintf('The correction force going through the inbound joint is:\n');
-%         epsilon
-%         fprintf('G is:\n');
-%         G
-%         fprintf('D is:\n');
-%         D
-%    end
 end
 
 end
