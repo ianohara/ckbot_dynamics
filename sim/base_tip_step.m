f6993737c77196b3b829b43a3460642cd4bedacb
Ian O'Hara <ian.ohara@gmail.com>
2012-01-29 12:24:24 -0800

    Comments explaining base to tip recursion.  Also, fixed a major gotcha bug: The joint matrix in the base to tip recursion was being used with components in the link frame instead of inertial. Fixing this fixed the simulation instability I was seeing earlier (woot!)

--- sim/base_tip_step.m
+++ sim/base_tip_step.m
@@ -4,12 +4,22 @@ function qdd = base_tip_step(chain, s, T, G, mu, a)
 % the tip_base_step for this step.
 %
 % ARGUMENTS:
-%  sim - the simulation structure.  See sim_doc.txt for documentation
+%   chain - array of links
+%   s - Current state [2N x 1]
+%   T - Current motor torques [N x 1]
+%   G - G calculated by tip_base_step(...) [6N x 1]
+%   mu - mu calculated by tip_base_step(...) [N x 1]
+%   a - a calculated by tip_base_step(...) [6N x 1]
 %
-% RETURNS:
-%  
-% qdd - nx1 array of joint accelerations
-
+% RETURNS:  
+%   qdd - nx1 array of joint accelerations
+%
+% TODO:
+%   Move get_chain_pos_rot(...) outside of the loop and change it to return
+%   a 3x3N block matrix where each 3x3 block is a rotation matrix for a
+%   link.  Then R_Cur inside the loop can be obtained by indexing this
+%   block matrix.
+%
 N = size(chain,1);
 
 alpha = zeros(6,1);  % The seed acceleration (ie: the base joint does not rotate
@@ -17,33 +27,37 @@ alpha = zeros(6,1);  % The seed acceleration (ie: the base joint does not rotate
                      % recursion)
 q = s(1:N);
 qd = s(N+1:end);
-qdd = NaN(N,1);
+qdd = NaN(N,1);  % We calculate this.
 
 chain = propogate_angles_and_rates(chain, q, qd);
 
 for i = 1:N
     cur = chain(i);
     
+    % Rotation of current link's coordinate frame to the inertial frame
     R_cur = get_chain_pos_rot(chain, i);
+    
+    % Vector from this link's inbound joint to its outbound joint
     r_i_ip = R_cur*(cur.r_ip1 - cur.r_im1);
-    phi = get_bod_trans(r_i_ip);   % From outbound to inbound
+    % Spatial tranformation operator from outbound joint to inbound joint
+    phi = get_bod_trans(r_i_ip);
   
-    alpha_p = phi'*alpha;  % inbound alpha transformed to outbound frame
+    % inbound alpha transformed to outbound frame (note phi transpose)
+    % for next time around loop
+    alpha_p = phi'*alpha;  
     
     p_ind = get_block_indicies(i);
     
     qdd(i) = mu(i) - G(p_ind)'*alpha_p;
     
-    H = get_joint_mat(cur);
-    alpha = alpha_p + H'*qdd(i) + a(p_ind);     
-% 
-%     if (exist('DEBUG_MSG') || exist('DEBUG_BASE_TIP'))
-%        fprintf('----------- LINK %d ------------\n', i);
-%        fprintf('Chain Description:\n (1=base, %d=tip)\n', N);
-%        fprintf('Spatial acceleration at this link''s joint:\n');
-%        print_vec(alpha);
-%         
-%     end
+    % Joint matrix.  H is in the inertial coordinate sys (like everything
+    % used in this algorithm should be)
+    H_b_frame_star = get_joint_mat(cur)';
+    H_w_frame_star = [R_cur, zeros(3);zeros(3),eye(3)]*H_b_frame_star;
+    H = H_w_frame_star';
+    
+    % Spatial acceleration of the link's inbound joint
+    alpha = alpha_p + H'*qdd(i) + a(p_ind);
 end
 
 end
