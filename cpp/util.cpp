ba98827e7ae2006fb35cd078e3c6b388c484935e
Ian O'Hara <ian.ohara@gmail.com>
2012-06-30 16:42:49 -0400

    Whoops, had not added util.cpp and util.hpp so some of the past commits are broken (including the merge to master!)

---
+++ cpp/util.cpp
@@ -0,0 +1,36 @@
+/*
+* CKBot Kinodynamic Planning with OMPL
+* Copyright (C) 2012 Ian O'Hara
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+*(at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include<iostream>
+
+#include<Eigen/Core>
+#include<Eigen/Dense>
+#include<Eigen/Geometry>
+
+namespace util
+{
+
+/* The stream operator of Eigen::Vector3d prints
+ * the vector vertically.  This prints it horizontally
+ */
+void
+flat_print(const Eigen::Vector3d &v, std::ostream& out)
+{
+    out << "[" << v[0] << ", " << v[1] << ", " << v[2] << "]" << std::endl;
+}
+}
