55f6f1e08e11099a04a17ad2afc53c7da45f4720
uriah <uriah@modlab-desktop.(none)>
2012-07-05 10:44:02 -0400

    Added the beginnings of module_interface -U

---
+++ python/module_interface/moduleIface.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+'''
+A quick interface for controlling the modules, there will be a json parser that
+will read in json files and send commands to the modules, most likely we will
+also have a way to read positions from the modules. 
+
+Long term we may have some synchronization method for the modules, so that we
+can download each modules motor plan and have them execute the plan in sync.
+
+Test packets:
+msg0 = '120' + pack('<h', 0).encode('hex').upper() + '00' 
+msg50 = '120' + pack('<h', 50).encode('hex').upper() + '00
+
+msg_param_autoZero_off = '620' + pack('B', 17).encode('hex').upper() +7*'00'
+msg_calibration = '120' + 2*'00' + pack('B',4).encode('hex').upper()
+'''
+
+import serial
+from time import time as now, sleep
+from struct import pack, unpack
+from string import find
+
+class moduleIface( object ):
+
+    NETWORK_MANAGEMENT = '000'
+    EMERGENCY = '0'
+    COMMAND = '1'
+    REQUEST_RESPONSE = '5'
+    REQUEST = '6'
+    HB = '7'
+
+    def __init__( self, dev='/dev/ttyUSB0' ):
+	ser = serial.Serial( dev )
+	ser.setBaudrate( 115200 )
+	ser.setTimeout( 0.001 )
+	ser.flush()
+	self.ser = ser
+	self.buf = ''
+
+    def read( self, tout = 0.01 ):
+	'''
+	Returns a single valid packet where a packet is defined as:
+	    'a-z',c for CANBus| len0 len1 | data .... | '\r'
+	'''
+	t0 = now()
+	while True:
+	    # If timeout return
+	    if now()-t0 > tout:
+		return None
+	    # Read a single byte
+	    b = self.ser.read()
+	    #print repr(b)
+	    #print repr(self.buf)
+	    # If no data then try to read again
+	    if b == '':
+		continue
+	    # If we haven't reached the end of the packet then append
+	    if b != '\r':
+		self.buf += b
+		continue
+	    # Parse the packet 
+	    while len(self.buf) > 0:
+		self.buf = self.buf[find(self.buf, 'c')+1:]
+		length = int(self.buf[:2], 16)
+		if len(self.buf)-2 == length:
+		    pkt = self.buf[2:]
+		    self.buf = ''
+		    return pkt
+    
+    def write( self, pkt ):
+	print repr(pkt)
+	msg = 'c' + pack('B',len(pkt)).encode('hex').upper() + pkt + '\r'
+	print repr(msg)
+	self.ser.write(msg)
+    
+    def set_voltage( self, val ):
+	msg = '120' + pack('<h', val).encode('hex').upper()	
+	self.write( msg ) 
+	
+ 
+	
+
+	    
+	
+	    
