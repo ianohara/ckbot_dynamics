7d1a45342e02d469ffe8cfc6d0373a2c5fb4dd57
uriah <uriahjb@gmail.com>
2012-09-10 20:14:52 -0400

    added some test files, and updates to posTest and plotTest

--- python/module_interface/posTest.py
+++ python/module_interface/posTest.py
@@ -3,8 +3,9 @@ import moduleIface
 from time import time as now, sleep 
 from math import pi
 from random import random
+import json
 
-m = moduleIface.moduleIface('/dev/ttyUSB1')
+m = moduleIface.moduleIface('/dev/ttyUSB0')
 
 ALL = 0
 HB = 2
@@ -14,56 +15,77 @@ def can_pass( m_id, passthis ):
     print repr(pass_pkt)
     m.ser.write(pass_pkt)
 
-test_time = 1
-
-pkt = '000'
-stop = 'f' + pack('B', len(pkt)).encode('hex').upper() + '100' + '\r'
-start = 'f' + pack('B', len(pkt)).encode('hex').upper() + '0' + pack('B',
-test_time).encode('hex').upper() + '\r'
-
-m.ser.write(start)
-
-cmd = 80
-
-#m.set_voltage(19, cmd) # for 4
-#m.set_voltage(38, cmd) # for 5
-#m.set_voltage(37, 40) # for 3
-
-#sleep( 0.1 )
 
 
-#for i in xrange(10):
-#    m.set_voltage(19, 0)
-#    m.set_voltage(38, 0)
-#for i in xrange(10):
-#    m.set_voltage(37, 0)
-sleep(test_time+1)
+def set_voltage( vol ):
+    for i in xrange(10):   
+        m.set_voltage(19,vol)
+        sleep(random()/10.0)
+
+def set_zero():
+    for i in xrange(10):   
+        m.set_voltage(19,0)
+        sleep(random()/10.0)
 
 
+def run_test( test_name, test_time ):
 
-msg = ''
-pkts = []
-while True:
-    b = m.ser.read()
-    if b == '':
-        break
-    msg += b
-    if b == '\r':
-        #print repr(msg)
-        pkts.append(msg)
-        msg = ''
+    pkt = '000'
+    stop = 'f' + pack('B', len(pkt)).encode('hex').upper() + '100' + '\r'
+    start = 'f' + pack('B', len(pkt)).encode('hex').upper() + '0' + pack('B',
+    test_time).encode('hex').upper() + '\r'
 
-m_dat = []
-for pkt in pkts:
-    if len(pkt) != 10:
-        continue
-    vals = unpack('<BhhB', pkt[3:-1])
-    m_dat.append(vals)
-    m_id = vals[0]
-    pos = vals[1]
-    #pos = 2.0*pi*vals[1]/2**15
-    #if pos > pi:
-    #    pos = pos - 2*pi
-    time = vals[2]/1000.0
-    voltage = float(vals[3])/2**5
-    print "ID: %d, Pos: %d, Time: %f, Voltage: %f" % (m_id, pos, time, voltage)
+    print "Running Test: %s" % test_name
+    print "Test time is: %f" % test_time
+
+    m.ser.write(start)
+
+    sleep(5.0)
+    set_voltage(200)
+    sleep(0.1)
+    set_zero()
+
+    sleep(test_time+1)
+
+    msg = ''
+    pkts = []
+    while True:
+        b = m.ser.read()
+        if b == '':
+            break
+        msg += b
+        if b == '\r':
+            #print repr(msg)
+            pkts.append(msg)
+            msg = ''
+
+    m_dat = []
+    modules = {}
+    for pkt in pkts:
+        if len(pkt) != 10:
+            continue
+        vals = unpack('<BhhB', pkt[3:-1])
+        m_dat.append(vals)
+        m_id = vals[0]
+        modules[m_id] = m_id
+        pos = vals[1]
+        pos_raw = pos 
+        if pos_raw > 2**14:
+            pos_raw = pos_raw - 2**15
+        position = pi*pos_raw/float(2**15)
+        #pos = 2.0*pi*vals[1]/2**15
+        #if pos > pi:
+        #    pos = pos - 2*pi
+        time = vals[2]/1000.0
+        voltage = float(vals[3])/2**5
+        print "ID: %d, pos_raw: %d, Pos: %f, Time: %f, Voltage: %f" % (m_id, pos, position, time, voltage)
+
+    test_dat = {'name': test_name, 'time':test_time, 'data':m_dat,
+    'modules':modules}
+    print "Saving test data to: %s" % test_name
+    f = open('tests/' + test_name, 'w')
+    json.dump(test_dat, f)
+    f.close()
+    print "Test successfully completed"
+
+
