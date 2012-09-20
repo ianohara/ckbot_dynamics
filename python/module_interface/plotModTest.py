2ed6b9f7199be97413b3ad226ba0e4617ba89766
uriah <uriahjb@gmail.com>
2012-09-20 09:47:20 -0400

    Added a whole ton of crap that was accidentally not added from the test computer.

---
+++ python/module_interface/plotModTest.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+import json
+from math import pi
+import numpy
+
+import matplotlib.pyplot as plt
+
+module_traj = {}
+
+module_param = {2: [21600, 5118, 30248],
+                3: [18880, 2350, 27216], 
+                4: [24868, 8530, 250], 
+                5: [23152, 6614, 31600]}
+
+test_name = 'tests/pt1_pulse_3module_outplane'
+
+test_dat = json.load( open( test_name, 'r' ))
+dat = test_dat['data']
+test_time = test_dat['time']
+test_name = test_dat['name']
+modules = test_dat['modules']
+if test_dat.has_key('date'):
+    print "Test date: %s" % test_dat['date']
+
+for k,v in module_param.items():
+    offs = ((2**15/4-v[1])+(2**15*3/4-v[0]))/2
+    module_param[k].append(offs)
+
+for dat_stub in dat:
+    module_id = dat_stub[0]
+    pos_raw = dat_stub[1]
+    if pos_raw > 2**14:
+        pos_raw = pos_raw - 2**15
+ 
+    max_tics = module_param[module_id][1]
+    min_tics = module_param[module_id][0] - 2**15
+    pos = -pi/2 + (pi/2--pi/2)/(max_tics-min_tics)*(pos_raw-min_tics)
+
+    pos = pos*5.0
+
+    time = dat_stub[2]
+    if time < 0:
+        continue
+    voltage = dat_stub[3]
+    if not module_traj.has_key(module_id):
+        module_traj[module_id] = [] 
+    module_traj[module_id].append([time, pos])
+
+for module in modules.values():
+    t_p = numpy.array(module_traj[module])
+    plt.plot(t_p[...,0],
+            t_p[...,1], 'o')
+    for i, t in enumerate(t_p[:,0]):
+        if abs(t - 5000.0) < 10:
+            print "Module ", module, " at time ", t, " is at ", t_p[i,1]
+            print "  With speed = ", 1000.0*(t_p[i+1,1] - t_p[i,1])/(t_p[i+1,0] - t_p[i, 0])
+
+
+plt.grid(True)
+plt.show()
