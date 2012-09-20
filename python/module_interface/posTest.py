20bae447b70157493355683638bc9e0e4b6f8329
Ian O'Hara <ian.ohara@gmail.com>
2012-09-20 14:40:21 -0400

    Added checks and re-organization to the constructor.  Now everythign is done through the constructor.

--- python/module_interface/posTest.py
+++ python/module_interface/posTest.py
@@ -1,99 +1,140 @@
-from struct import pack, unpack
-import moduleIface
-import time
+import time, json, sys
 from math import pi
 from random import random
-import json
+from struct import pack, unpack
 
-m = moduleIface.moduleIface('/dev/ttyUSB1')
+import moduleIface
 
-ALL = 0
-HB = 2
-NONE = 1
+class PositionLogger(object):
+    """
+    Class that listens to a serial line on which
+    parkourbot modules are reporting their positions.
+    This parses that data and stores it as json along with
+    as much information about the test as possible.
+    """
+    ALL = 0
+    HB = 2
+    NONE = 1
 
-def can_pass( m_id, passthis ):
-    pass_pkt = 'w' + pack('BBB', 4, m_id, passthis).encode('hex').upper() + '\r'
-    print repr(pass_pkt)
-    m.ser.write(pass_pkt)
+    def __init__(self,
+                 dev='/dev/ttyUSB1',
+                 modules=None,   # List of modules to listen to
+                 jsonout=dict(), # Json dictionary to store results and stup
+                 test_name="PositionLogger_" + str(int(time.time())),
+                 test_time=None, # Len in [s] of trajectory
+                 debug=False     # Use debug output?
+                 ):
+        """
+        dev     - device string of serial device associated
+                  with the module network.
+        modules - list of module IDs in the chain (Brain board IDs)
+        debug   - Boolean indicating whether debug output should be printed
+        jsonout - dictionary to use as the json output object for this test.
+                  This could be used to write the result and setup of this
+                  test to file, so fill it with all of the information needed
+                  to recreate it.
+        """
+        try:
+            self.m = moduleIface.moduleIface('/dev/ttyUSB1')
+        except Exception as e:
+            print "Cannot establish serial connection with device at '%s'" % dev
+            raise e
+        self.jsonout = jsonout
 
-def set_voltage( vol ):
-    for i in xrange(10):
-        m.set_voltage(19,vol)
-        time.sleep(random()/10.0)
+        if (not self.jsonout.get('results')):
+            self.jsonout.setdefault('results', dict())
 
-def set_zero():
-    for i in xrange(10):
-        m.set_voltage(19,0)
-        time.sleep(random()/10.0)
-    for i in xrange(10):
-        m.set_voltage(38,0)
-        time.sleep(random()/10.0)
-    for i in xrange(10):
-        m.set_voltage(37,0)
-        time.sleep(random()/10.0)
+        if not modules:
+            raise Exception("PositionLogger: Must specify modules to listen to.")
 
-def run_test( test_name, test_time ):
+        if not test_name:
+            test_name = "PositionLogger_" + str(int(time.time()))
+        self.test_name = test_name
 
-    pkt = '000'
-    stop = 'f' + pack('B', len(pkt)).encode('hex').upper() + '100' + '\r'
-    start = 'f' + pack('B', len(pkt)).encode('hex').upper() + '0' + pack('B',
-    test_time).encode('hex').upper() + '\r'
+        if not test_time:
+            raise Exception("PositionLogger: Must specify a test length with test_time paramter")
+        self.test_time = test_time
 
-    print "Running Test: %s" % test_name
-    print "Test time is: %f" % test_time
+        self.modules = modules
+        self.data = list()
 
-    m.ser.write(start)
+    def can_pass( self, m_id, passthis ):
+        pass_pkt = 'w' + pack('BBB', 4, m_id, passthis).encode('hex').upper() + '\r'
+        if self.debug: print repr(pass_pkt)
+        self.m.ser.write(pass_pkt)
 
-    #time.sleep(0.5)
-    #set_voltage(200)
-    #sleep(0.1)
-    #set_zero() # 6 -> 17
+    def set_voltage( self, vol ):
+        for i in xrange(10):
+            self.m.set_voltage(19,vol)
+            time.sleep(random()/10.0)
 
-    time.sleep(test_time+1)
+    def set_zero(self):
+        for i in xrange(10):
+            self.m.set_voltage(19,0)
+            time.sleep(random()/10.0)
+        for i in xrange(10):
+            self.m.set_voltage(38,0)
+            time.sleep(random()/10.0)
+        for i in xrange(10):
+            self.m.set_voltage(37,0)
+            time.sleep(random()/10.0)
 
-    msg = ''
-    pkts = []
-    while True:
-        b = m.ser.read()
-        if b == '':
-            break
-        msg += b
-        if b == '\r':
-            #print repr(msg)
-            pkts.append(msg)
-            msg = ''
+    def run_test(self):
+        pkt = '000'
+        stop = 'f' + pack('B', len(pkt)).encode('hex').upper() + '100' + '\r'
+        start = 'f' + pack('B', len(pkt)).encode('hex').upper() + '0' + pack('B',
+                self.test_time).encode('hex').upper() + '\r'
 
-    m_dat = []
-    modules = {}
-    for pkt in pkts:
-        if len(pkt) != 10:
-            continue
-        vals = unpack('<BHHB', pkt[3:-1])
-        m_dat.append(vals)
-        m_id = vals[0]
-        modules[m_id] = m_id
-        pos = vals[1]
-        if pos > 2**15:
-            print "ID: %d, Pos greater than 2**15" % m_id
-            continue
-        pos_raw = pos
-        if pos_raw > 2**14:
-            pos_raw = pos_raw - 2**15
-        position = pi*pos_raw/float(2**15)
-        #pos = 2.0*pi*vals[1]/2**15
-        #if pos > pi:
-        #    pos = pos - 2*pi
-        pkt_time = vals[2]/1000.0
-        voltage = float(vals[3])/2**5
-        print "ID: %d, pos_raw: %d, Pos: %f, Time: %f, Voltage: %f" % (m_id,
-        pos, position, pkt_time, voltage)
+        print "Running Test: %s" % self.test_name
+        print "Test time is: %f" % self.test_time
 
-    time_str = time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.localtime())
-    test_dat = {'date': time_str, 'name': test_name, 'time':test_time, 'data':m_dat,
-    'modules':modules}
-    print "Saving test data to: %s" % test_name
-    f = open('tests/' + test_name, 'w')
-    json.dump(test_dat, f)
-    f.close()
-    print "Test successfully completed"
+        self.m.ser.write(start)
+
+        time.sleep(self.test_time+1)
+
+        msg = ''
+        pkts = list()
+        while True:
+            b = self.m.ser.read()
+            if b == '':
+                break
+            msg += b
+            if b == '\r':
+                pkts.append(msg)
+                msg = ''
+        for pkt in pkts:
+            if len(pkt) != 10:
+                if self.debug: print "Found a faulty packet..."
+                continue
+            # TODO(IMO): What's the packet format?
+            vals = unpack('<BHHB', pkt[3:-1])
+            self.data.append(vals)
+            m_id = vals[0]
+            modules[m_id] = m_id
+            pos = vals[1]
+            if pos > 2**15:
+                print "ID: %d, Pos greater than 2**15" % m_id
+                continue
+            pos_raw = pos
+            if pos_raw > 2**14:
+                pos_raw = pos_raw - 2**15
+            position = pi*pos_raw/float(2**15)
+            pkt_time = vals[2]/1000.0
+            voltage = float(vals[3])/2**5
+            print "ID: %d, pos_raw: %d, Pos: %f, Time: %f, Voltage: %f" % (m_id,
+            pos, position, pkt_time, voltage)
+
+        time_str = time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.localtime())
+
+        self.jsonout['results']['date'] = time_str
+        self.jsonout['results']['name'] = self.test_name
+        self.jsonout['results']['time'] = self.test_time
+        self.jsonout['results']['data'] = self.data
+        self.jsonout['results']['modules'] = self.modules
+
+        print "Saving test data to: %s" % self.test_name
+        f = open('tests/' + self.test_name, 'w')
+        json.dump(self.jsonout, f)
+        f.close()
+        print "Test successfully completed"
 
